0f9be8b30fc2c5bc7e1429015d43c71500727e54
LemayC <lemay.charles21@gmail.com>
2016-10-31 15:02:42 -0400

    Maudit que c'est plate holy shit

---
+++ app/src/main/java/ca/qc/cegepdrummondville/applicationdehockey/MainActivity.java
@@ -0,0 +1,13 @@
+package ca.qc.cegepdrummondville.applicationdehockey;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
