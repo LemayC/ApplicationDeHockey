771654614d151f87bd055ecd92a8bb1880cecf8d
LemayC <lemay.charles21@gmail.com>
2016-11-15 15:48:53 -0500

    solitaire...

--- app/src/main/java/ca/qc/cegepdrummondville/applicationdehockey/MainActivity.java
+++ app/src/main/java/ca/qc/cegepdrummondville/applicationdehockey/MainActivity.java
@@ -2,12 +2,20 @@ package ca.qc.cegepdrummondville.applicationdehockey;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    MySQLiteHelper sqliteHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sqliteHelper = new MySQLiteHelper(this);
+    }
+
+    public void addPenalty(View view) {
+        Penalty newPenalty = new Penalty("A", 10, 10, 10);
+        sqliteHelper.addPenalty(newPenalty);
     }
 }
